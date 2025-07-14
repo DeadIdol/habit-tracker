33b046659d1d9dbc8499edd1c4a4649130aacbf5
DeadIdol <s4000045@student.rmit.edu.au>
2025-07-14 20:10:18 +1000

    Remove help button

--- src/components/MainTab.tsx
+++ src/components/MainTab.tsx
@@ -1,31 +1,17 @@
 import { useHabits } from "@/context/HabitContext"
 import HabitCard from "./HabitCard";
-import { Collapsible } from "@/components/ui/collapsible";
 import { useState } from "react";
-import { HelpCircle } from "lucide-react";
 import { Button } from "./ui/button";
-import { CollapsibleContent, CollapsibleTrigger } from "@radix-ui/react-collapsible";
 import { Input } from "./ui/input";
 import { getCurrentDateString } from "@/util/date_utils";
 
 export default function MainTab() {
   const { habits, setHabits } = useHabits();
-  const [helpOpen, setHelpOpen] = useState(false);
   const [dateString, setDateString] = useState(getCurrentDateString());
 
   return (
     <div className="flex flex-col items-center space-y-3 ">
-      <h1 className="text-4xl text-center">Welcome to Habit Tracker</h1>
-      <Collapsible open={helpOpen} onOpenChange={setHelpOpen}>
-        <CollapsibleTrigger asChild>
-          <Button size='icon'>
-            <HelpCircle />
-          </Button>
-        </CollapsibleTrigger>
-        <CollapsibleContent>
-          
-        </CollapsibleContent>
-      </Collapsible>
+      
 
       <Input type='date' value={dateString} onChange={(e) => setDateString(e.target.value)} className="w-min"></Input>
       <Button
