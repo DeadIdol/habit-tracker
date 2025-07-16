9067b9bdd9a142138d77c0eb13a7459dc770a4c8
DeadIdol <s4000045@student.rmit.edu.au>
2025-07-16 11:22:51 +1000

    Using react-dnd-preview

--- src/components/MainTab.tsx
+++ src/components/MainTab.tsx
@@ -1,5 +1,6 @@
 import { useHabits } from "@/context/HabitContext"
-import HabitCard from "./HabitCard";
+import DraggableHabitCard from "./DraggableHabitCard"
+import HabitCard from "./HabitCard"
 import { useCallback, useState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
@@ -7,6 +8,10 @@ import { getCurrentDateString } from "@/util/date_utils";
 import Habit from "@/types/Habit";
 
 import update from 'immutability-helper'
+import { Preview } from 'react-dnd-preview'
+import { ItemTypes } from "@/types/ItemTypes";
+import { GripVertical } from "lucide-react";
+
 
 
 export default function MainTab() {
@@ -27,7 +32,7 @@ export default function MainTab() {
   const renderCard = useCallback(
     (habit: Habit, index: number) => {
       return (
-        <HabitCard
+        <DraggableHabitCard
           id={habit.id}
           habit={habit}
           key={habit.id}
@@ -36,7 +41,7 @@ export default function MainTab() {
           moveCard={moveCard}
         />
       )
-    }, [])
+    }, [dateString, moveCard])
 
   return (
     <div className="flex flex-col items-center space-y-3 ">
@@ -62,6 +67,28 @@ export default function MainTab() {
           renderCard(habit, i)
         ))}
       </div>
+      <Preview generator={({itemType, item, style}) => {
+        if (itemType !== ItemTypes.HABIT_CARD) return null
+        console.log('preview generated')
+        return (
+          <div style={{...style, boxShadow: "0 4px 16px rgba(0,0,0,0.15)", background: "white", borderRadius: 8, padding: 0, margin: 0}}>
+            <div className="flex flex-row">
+              <button
+                className="border rounded-md w-6 place-content-center cursor-grabbing"
+                style={{marginRight: 0}}
+              >
+                <GripVertical />
+              </button>
+              <HabitCard
+                id={item.id}
+                habit={habits[item.index]}
+                key={item.id}
+                date={dateString}
+              />
+            </div>
+          </div>
+        )
+      }} />
     </div>
   )
 }
